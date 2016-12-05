dbd47e6c1f2e2120584b0baae845711bf85a209f
Andreas Breuninger <a.breuninger@gmx.net>
2016-12-05 19:00:24 +0100

    played with extras on app start

--- app/main.ts
+++ app/main.ts
@@ -2,10 +2,32 @@
 import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
 import { AppModule } from './modules/app.module';
 import app = require('application');
+import { AppRoutingModule } from './modules/app-routing.module';
+import { RouterExtensions } from 'nativescript-angular';
+
 
 app.on(app.launchEvent, function (args: app.ApplicationEventData) {
     // on launch code
-    console.log(args);
+    console.log(args.android);
+    var launchIntent = args.android;
+
+    var extra = launchIntent.getStringExtra("params");
+    console.log(extra);
+
+    // if (extra) {
+    //     RouterExtensions.prototype.navigateByUrl("launch");
+    // }
+
+});
+
+app.on(app.uncaughtErrorEvent, function (args: app.ApplicationEventData) {
+    if (args.android) {
+        // For Android applications, args.android is an NativeScriptError.
+        console.log("NativeScriptError: " + args.android);
+    } else if (args.ios) {
+        // For iOS applications, args.ios is NativeScriptError.
+        console.log("NativeScriptError: " + args.ios);
+    }
 });
 
 
