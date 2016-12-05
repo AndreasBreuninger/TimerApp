f4779f982f6eb793615ff57fe2cb2862b0413f6d
Andreas Breuninger <a.breuninger@gmx.net>
2016-12-05 18:11:32 +0100

    added app start event

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,12 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
-
 import { AppModule } from './modules/app.module';
+import app = require('application');
+
+app.on(app.launchEvent, function (args: app.ApplicationEventData) {
+    // on launch code
+    console.log(args);
+});
+
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
